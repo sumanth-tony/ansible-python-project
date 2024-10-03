4ad9ccc7d91798a05eee60b95cb8efb5f91c5518
sumanth <sumanth1119006@gmail.com>
2024-10-03 11:30:37 +0530

    localhost changes

--- scripts/automate.py
+++ scripts/automate.py
@@ -1,13 +1,20 @@
 import subprocess
+import os
+
+# Get the current working directory and construct paths to playbooks and inventory
+current_directory = os.path.dirname(os.path.abspath(__file__))
+webserver_playbook = os.path.join(current_directory, '../ansible/webserver.yml')
+dbserver_playbook = os.path.join(current_directory, '../ansible/dbserver.yml')
+inventory = os.path.join(current_directory, '../ansible/inventory.ini')
 
 def run_playbook(playbook):
     try:
-        # Execute the ansible playbook command
-        result = subprocess.run(['ansible-playbook', playbook], check=True, stdout=subprocess.PIPE)
+        # Execute the ansible-playbook command with the inventory file
+        result = subprocess.run(['ansible-playbook', '-i', inventory, playbook], check=True, stdout=subprocess.PIPE)
         print(result.stdout.decode('utf-8'))
     except subprocess.CalledProcessError as e:
         print(f"Error occurred: {e}")
 
 # Trigger web server and database playbooks
-run_playbook('webserver.yml')
-run_playbook('dbserver.yml')
+run_playbook(webserver_playbook)
+run_playbook(dbserver_playbook)
